e54ff2e51cf534471e8ae6fefdbd8cd046cac67c
Lakr Aream <master@233owo.com>
2020-01-31 23:20:20 +0800

    添加工具箱选项

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 import { iDeviceNodeProvider, iDeviceItem } from './iDeviceConnections';
+import { ToolboxNodeProvider } from './iDeviceToolbox';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Bootstraping "wiki.qaq.iosre" extension!');
 
 	iDeviceNodeProvider.init();
+	ToolboxNodeProvider.init();
 
 	let disposable = vscode.commands.registerCommand('extension.iOSreAction-ShowVersion', () => {
 		vscode.window.showInformationMessage("wiki.qaq.iosre -> Version 0.1");
---
+++ src/iDeviceToolbox.ts
@@ -0,0 +1,57 @@
+import * as vscode from 'vscode';
+import { join } from 'path';
+import { LKutils } from './Utils';
+
+export class ToolItem extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+		private version: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+        super(label, collapsibleState);
+        this.iconPath = this.getToolIcon(label);
+    }
+
+    private getToolIcon(name: String): vscode.Uri {
+        if (name === "SSH Connect") {
+            return vscode.Uri.file(join(__filename,'..', '..' ,'res' ,'shell.svg'));
+        } else if (name === "Copy UDID" || name === "Copy ECID" ) {
+            return vscode.Uri.file(join(__filename,'..', '..' ,'res' ,'copy.svg'));
+        }  else if (name === "Show Details" ) {
+            return vscode.Uri.file(join(__filename,'..', '..' ,'res' ,'info.svg'));
+        } else {
+            return vscode.Uri.file(join(__filename,'..', '..' ,'res' ,'ios.svg'));
+        }
+    }
+}
+
+export class ToolboxNodeProvider implements vscode.TreeDataProvider<ToolItem> {
+
+    public static tools = ["SSH Connect", "Copy UDID", "Copy ECID", "Show Details"];
+    public static nodeProvider: ToolboxNodeProvider;
+
+    public static init() {
+        const np = new ToolboxNodeProvider();
+        vscode.window.registerTreeDataProvider('iosreIDtabSectionToolboxs', np);
+        this.nodeProvider = np;
+    }
+
+	private _onDidChangeTreeData: vscode.EventEmitter<ToolItem> = new vscode.EventEmitter<ToolItem>();
+    readonly onDidChangeTreeData: vscode.Event<ToolItem | undefined> = this._onDidChangeTreeData.event;
+
+    getTreeItem(element: ToolItem): vscode.TreeItem {
+        return element;
+    }
+
+    refresh() {
+        ToolboxNodeProvider.nodeProvider._onDidChangeTreeData.fire();
+    }
+
+    getChildren(element?: ToolItem | undefined): vscode.ProviderResult<ToolItem[]> {
+        return ToolboxNodeProvider.tools.map (
+            item => new ToolItem(item, item, vscode.TreeItemCollapsibleState.None)
+        );
+    }
+}
