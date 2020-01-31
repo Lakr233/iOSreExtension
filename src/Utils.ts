c318ebc8cf209d871fe0b97bb4877a741468a6d5
Lakr Aream <master@233owo.com>
2020-01-31 22:00:36 +0800

    ---

--- src/Utils.ts
+++ src/Utils.ts
@@ -11,7 +11,7 @@ export class LKutils {
             const cp = require('child_process');
             cp.exec('idevice_id -l', (err: string, stdout: string, stderr: string) => {
                 if (err) {
-                    vscode.window.showErrorMessage("iOSre -> EXECUTE_COMMAND_ERROR <- stderr:%s stdout:%s => %s", stderr, stdout, cmd);
+                    vscode.window.showErrorMessage("iOSre -> EXECUTE_COMMAND_ERROR -> stderr:" + stderr + " -> stdout:" + stdout + " -> whenExec:" + cmd + "  ==> Install dependency may solve the problem.");
                 }
                 resolve(stdout);
             });
